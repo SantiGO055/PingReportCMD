5fc39ac0d0bc6104206abea2aae1da150940fbed
SantiGO055 <santigonzalez05@gmail.com>
2021-08-26 12:33:20 -0300

    subo version final

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace FormCMD
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtDos = new System.Windows.Forms.TextBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtUno = new System.Windows.Forms.TextBox();
+            this.txtTres = new System.Windows.Forms.TextBox();
+            this.txtCuatro = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtDos
+            // 
+            this.txtDos.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtDos.Location = new System.Drawing.Point(113, 21);
+            this.txtDos.MaxLength = 3;
+            this.txtDos.Name = "txtDos";
+            this.txtDos.Size = new System.Drawing.Size(25, 20);
+            this.txtDos.TabIndex = 2;
+            this.txtDos.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDos_KeyPress);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(113, 61);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(69, 26);
+            this.btnOk.TabIndex = 5;
+            this.btnOk.Text = "Ping";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(100, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(10, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ".";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = ".";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(188, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(10, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = ".";
+            // 
+            // txtUno
+            // 
+            this.txtUno.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtUno.Location = new System.Drawing.Point(69, 21);
+            this.txtUno.MaxLength = 3;
+            this.txtUno.Name = "txtUno";
+            this.txtUno.Size = new System.Drawing.Size(25, 20);
+            this.txtUno.TabIndex = 1;
+            this.txtUno.TextChanged += new System.EventHandler(this.txtUno_TextChanged);
+            this.txtUno.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUno_KeyDown);
+            this.txtUno.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtUno_KeyPress);
+            // 
+            // txtTres
+            // 
+            this.txtTres.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtTres.Location = new System.Drawing.Point(157, 21);
+            this.txtTres.MaxLength = 3;
+            this.txtTres.Name = "txtTres";
+            this.txtTres.Size = new System.Drawing.Size(25, 20);
+            this.txtTres.TabIndex = 3;
+            this.txtTres.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtTres_KeyPress);
+            // 
+            // txtCuatro
+            // 
+            this.txtCuatro.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtCuatro.Location = new System.Drawing.Point(204, 21);
+            this.txtCuatro.MaxLength = 3;
+            this.txtCuatro.Name = "txtCuatro";
+            this.txtCuatro.Size = new System.Drawing.Size(25, 20);
+            this.txtCuatro.TabIndex = 4;
+            this.txtCuatro.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtCuatro_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "IP:";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(103, 93);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(88, 26);
+            this.btnCancelar.TabIndex = 10;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.ClientSize = new System.Drawing.Size(291, 146);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCuatro);
+            this.Controls.Add(this.txtTres);
+            this.Controls.Add(this.txtUno);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDos);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "PingCMD";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDos;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtUno;
+        private System.Windows.Forms.TextBox txtTres;
+        private System.Windows.Forms.TextBox txtCuatro;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using Microsoft.Toolkit.Uwp.Notifications;
+
+namespace FormCMD
+{
+    public partial class Form1 : Form
+    {
+        private int uno;
+        private int dos;
+        private int tres;
+        private int cuatro;
+        private bool notif = false;
+        public Process proc;
+        Thread t;
+        delegate void CambiarProgresoDelegado(string texto, int valor);
+        public Form1()
+        {
+            InitializeComponent();
+            t = new Thread(new ParameterizedThreadStart(executeCommand));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            uno = ValidarNumero(txtUno.Text);
+            dos = ValidarNumero(txtDos.Text);
+            tres = ValidarNumero(txtTres.Text);
+            cuatro = ValidarNumero(txtCuatro.Text);
+            //FormCancelar formCancelar = new FormCancelar();
+
+            //formCancelar.ShowDialog();
+            if (ValidarNumerosIngresados(uno, dos, tres, cuatro))
+            {
+                string ip;
+                ip = uno + "." + dos + "." + tres + "." + cuatro;
+                //this.Hide();
+                if (txtDos.TextLength > 0)
+                {
+                    btnOk.Enabled = false;
+                    t.Start("ping -t "+ ip);
+                }
+                    //executeCommand("ping -t " + ip);
+            }
+            else
+            {
+                MessageBox.Show("Error al ingresar la ip", "Error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+        }
+        private bool checkLostPackages()
+        {
+
+            return true;
+        }
+        private int ValidarNumero(string numero)
+        {
+            if (int.TryParse(numero,out int numeroInt))
+            {
+                return numeroInt;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        private bool ValidarNumerosIngresados(int uno, int dos, int tres, int cuatro)
+        {
+            if(uno != 0 && dos != 0 && tres != 0 && cuatro != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private void executeCommand(object comando)
+        {
+            
+            string commandR = (string)comando;
+
+            
+            try
+            {
+                //if (commandR == "exit")
+                //{
+                //    proc.StandardInput.Close();
+                //}
+                proc = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "cmd.exe",
+                        Arguments = "/c " + commandR,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+                updateStatusExecution("Comando: " + commandR);
+                proc.Start();
+
+
+                while (!proc.StandardOutput.EndOfStream)
+                {
+
+                    string line = proc.StandardOutput.ReadLineAsync().Result;
+                    if(line != null)
+                    {
+                        if (line.ToString().Contains("Haciendo ping a") || line.ToString().Contains("Respuesta desde") || String.IsNullOrEmpty(line) )
+                        {
+                            
+                        }
+                        else
+                        {
+                            //MessageBox.Show(line.ToString(), "Prueba", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            if (!this.notif)
+                            {
+                                Uri uriImg = new Uri("https://cdn-0.emojis.wiki/emoji-pics/facebook/pensive-face-facebook.png");
+                                //this.notif = true;
+                                //enviar notificacion de perdida de paquete
+                                new ToastContentBuilder()
+                                .AddAppLogoOverride(uriImg)
+                                .AddArgument("action", "viewConversation")
+                                .AddArgument("conversationId", 9813)
+                                .AddText("Se ha perdido paquetes")
+                                .AddText("Se perdieron paquetes " + comando)
+                                .Show(); // Not seeing the Show() method? Make sure you have version 7.0, and if you're using .NET 5, your TFM must be net5.0-windows10.0.17763.0 or greater
+                            }
+                            
+
+                        }
+
+                    }
+                    updateStatusExecution(line);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                updateStatusExecution("***Error while executing '" + commandR + "'" + " " + e);
+                MessageBox.Show("El reporte ha finalizado y ha sido guardado en C:/reporte.txt", "Aviso!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+
+        }
+
+        private void updateStatusExecution(String textR)
+        {
+            String currentDateTime = DateTime.Now.ToString(); ;
+            
+            guardarArchivo(textR);
+            
+        }
+        private void guardarArchivo(string sb)
+        {
+            try
+            {
+                Directory.Exists(@"C:\reporte.txt");
+
+                string[] arrayDirectorio = Directory.GetFiles(@"C:\", "reporte*.*");
+
+                string pathApp = "C:\\";
+
+                using (StreamWriter sw = new StreamWriter(@"C:\reporte.txt", true))
+                {
+                    sw.WriteLine("--------------------------");
+                    sw.Write(sb);
+                    sw.WriteLine(" | " + DateTime.Now);
+                }
+
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private void txtUno_KeyDown(object sender, KeyEventArgs e)
+        {
+            
+            //if (e.KeyCode.Equals(Keys.))
+            //{
+            //    textBox1.Focus();
+            //}
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            executeCommand("exit");
+            Application.Exit();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (t.IsAlive)
+                t.Abort();
+            
+        }
+
+        private void txtUno_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void txtUno_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+                    {
+                        e.Handled = true;
+            }
+            if (e.KeyChar == '.'){
+                e.Handled = true;
+            }
+            if (e.KeyChar == 46)
+            {
+
+                txtDos.Focus();
+            }
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnOk_Click(this, new EventArgs());
+            }
+        }
+
+        private void txtDos_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == '.')
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 46)
+            {
+
+                txtTres.Focus();
+            }
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnOk_Click(this, new EventArgs());
+            }
+        }
+
+        private void txtTres_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == '.')
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 46)
+            {
+
+                txtCuatro.Focus();
+            }
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnOk_Click(this, new EventArgs());
+            }
+        }
+
+        private void txtCuatro_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == '.')
+            {
+                e.Handled = true;
+            }
+            if (e.KeyChar == 46)
+            {
+
+                txtUno.Focus();
+            }
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                btnOk_Click(this, new EventArgs());
+            }
+        }
+    }
+}
